ab5314f3767390c2562f95c3e1a0334e931b0419
Bernardo Rittmeyer <bernardo@rittme.com>
2015-03-13 11:35:07 +0100

    more fierce message

--- src/main.cpp
+++ src/main.cpp
@@ -80,7 +80,7 @@ int main(int argc, char ** argv) {
   StateMachine state_machine = StateMachine(input_path);
 
 
-  /*                THERE BE DRAGONS :
+  /*       Grrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
                          ^\    ^
                         / \\  / \
                        /.  \\/   \      |\___/|
