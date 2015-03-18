2ff96f29b2dfa6ab458a4a27ff7018335fce59d8
Edouard Oger <edouard.oger@gmail.com>
2015-03-18 18:33:03 +0100

    switch to boost regex

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,26 +1,26 @@
-#include <regex>
+#include <boost/regex.hpp>
 #include <fstream>
 #include <sstream>
 #include "lexer.h"
 #include "symboltype.h"
 
-static std::pair<SymbolType, std::regex> regexes[] = {
-    make_pair(SymbolType::AFF, std::regex("^:=")),
-    make_pair(SymbolType::PV,  std::regex("^;")),
-    make_pair(SymbolType::VG,  std::regex("^,")),
-    make_pair(SymbolType::EQ,  std::regex("^=")),
-    make_pair(SymbolType::ADD, std::regex("^+")),
-    make_pair(SymbolType::SUB, std::regex("^-")),
-    make_pair(SymbolType::MUL, std::regex("^*")),
-    make_pair(SymbolType::DIV, std::regex("^/")),
-    make_pair(SymbolType::PO,  std::regex("^\\(")),
-    make_pair(SymbolType::PF,  std::regex("^)")),
-    make_pair(SymbolType::VAR, std::regex("^var")),
-    make_pair(SymbolType::CST, std::regex("^const")),
-    make_pair(SymbolType::W,   std::regex("^ecrire")),
-    make_pair(SymbolType::R,   std::regex("^lire")),
-    make_pair(SymbolType::ID,  std::regex("^[a-zA-Z][a-zA-Z0-9]*")),
-    make_pair(SymbolType::VAL, std::regex("^[0-9]+"))
+static std::pair<SymbolType, boost::regex> regexes[] = {
+    std::make_pair(SymbolType::AFF, boost::regex("^:=")),
+    std::make_pair(SymbolType::PV,  boost::regex("^;")),
+    std::make_pair(SymbolType::VG,  boost::regex("^,")),
+    std::make_pair(SymbolType::EQ,  boost::regex("^=")),
+    std::make_pair(SymbolType::ADD, boost::regex("^\\+")),
+    std::make_pair(SymbolType::SUB, boost::regex("^-")),
+    std::make_pair(SymbolType::MUL, boost::regex("^\\*")),
+    std::make_pair(SymbolType::DIV, boost::regex("^/")),
+    std::make_pair(SymbolType::PO,  boost::regex("^\\(")),
+    std::make_pair(SymbolType::PF,  boost::regex("^\\)")),
+    std::make_pair(SymbolType::VAR, boost::regex("^var")),
+    std::make_pair(SymbolType::CST, boost::regex("^const")),
+    std::make_pair(SymbolType::W,   boost::regex("^ecrire")),
+    std::make_pair(SymbolType::R,   boost::regex("^lire")),
+    std::make_pair(SymbolType::ID,  boost::regex("^[a-zA-Z][a-zA-Z0-9]*")),
+    std::make_pair(SymbolType::VAL, boost::regex("^[0-9]+"))
 };
 
 Lexer::Lexer(std::string path) {
@@ -40,10 +40,10 @@ std::shared_ptr<Symbol> Lexer::getSymbol() {
     return m_curSymbol;
   }
 
-  std::smatch sm;
+  boost::smatch sm;
 
   for(const auto& reg: regexes) {
-    if(std::regex_match(m_content, sm, reg.second)) {
+    if(boost::regex_match(m_content, sm, reg.second)) {
 
       auto symbol = std::make_shared<Symbol>(reg.first, sm[0]);
       m_curSymbol = symbol;
