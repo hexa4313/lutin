0180099f971fda2dfc58092fecea26a781fcd7bf
Edouard Oger <edouard.oger@gmail.com>
2015-03-25 20:44:23 +0100

    Delegate getIdentifier() in ExpPar to the nested Expression

--- src/parsing/ast/instruction/exppar.cpp
+++ src/parsing/ast/instruction/exppar.cpp
@@ -5,7 +5,5 @@ void ExpPar::toString(std::ostream &o) const {
 }
 
 std::vector<std::string> ExpPar::getIdentifiers() {
-  std::vector<std::string> vars;
-  vars.insert(vars.end(), m_expr->getIdentifiers().begin(), m_expr->getIdentifiers().end());
-  return vars;
+  return m_expr->getIdentifiers();
 }
