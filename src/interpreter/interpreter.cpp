05a9c4c6ea1d7f1ee0f485b16b67e6ff02461039
Bernardo Rittmeyer <bernardo@rittme.com>
2015-03-26 08:16:07 +0100

    l'autre con

--- src/interpreter/interpreter.cpp
+++ src/interpreter/interpreter.cpp
@@ -2,11 +2,12 @@
 
 void Interpreter::init() {
   m_program->getDecList()->getDeclarationTable(m_table);
+  std::cout << "Execution\n";
   m_table.print();
 }
 
 bool Interpreter::step() {
-
+  return true;
 }
 
 void Interpreter::run() {
