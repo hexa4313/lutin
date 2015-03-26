86e6df7fb966d51a91c8ce6335c2863c3029cca9
Edouard Oger <edouard.oger@gmail.com>
2015-03-26 13:54:46 +0100

    fixed regression on backwards affectation searching

--- src/parsing/ast/instruction/instructionlist.cpp
+++ src/parsing/ast/instruction/instructionlist.cpp
@@ -12,10 +12,10 @@ void InstructionList::toString(std::ostream &o) const {
 
 std::shared_ptr<Instruction> InstructionList::lastAssignation(std::string id, std::shared_ptr<Instruction> instruction) {
 
-  auto it = std::find(m_instructions.begin(), m_instructions.end(), instruction);
+  auto it = std::find(m_instructions.rbegin(), m_instructions.rend(), instruction);
 
   // Search backwards
-  for(it--; it != m_instructions.end(); --it) {
+  for(it++; it != m_instructions.rend(); ++it) {
     auto prevInstruction = *it;
     if(prevInstruction->getType() == SymbolType::I_R) {
       auto r = std::dynamic_pointer_cast <Read> (prevInstruction);
